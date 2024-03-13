10d669ca701e5e3374881d05aae53cfe06de2938
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-03-13 09:21:59 -0300

    1rst Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Bienvenido</h1>
+    <p>Esta es la página de inicio.</p>
+    <a href="login.php"><button>Iniciar sesión</button></a>
+</body>
+</html>
